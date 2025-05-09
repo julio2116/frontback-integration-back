10d287158ca64b77fbefd772f7b8fefbcb4e5025
julio2116 <juliobatista2116@gmail.com>
2025-05-09 12:34:03 -0300

    implementacao de consumo da rota de alteracao de itens

--- front/js/alterItem.js
+++ front/js/alterItem.js
@@ -1,7 +1,7 @@
 function alterItem({id, imagem, nome, preco}){
     const view = document.querySelector('#view');
     const form = document.createElement('form');
-    view.appendChild(form);
+    view.appendChild(form).setAttribute('id', 'formAlter');
     const formEl = view.querySelector('form');
 
     for(const [key, value] of Object.entries({imagem, nome, preco})){
@@ -15,10 +15,35 @@ function alterItem({id, imagem, nome, preco}){
 
         div.appendChild(input).setAttribute('id', `${id}-${key}`);
         const inputEl = document.querySelector(`#${id}-${key}`);
+        inputEl.setAttribute('name', key);
 
+        
         inputEl.value = value;
     }
+    formEl.appendChild(document.createElement('button')).textContent = 'submit'
+    
+    async function patchItem(){
+        const formData = document.querySelector('#formAlter');
+        const objectData = new FormData(formData)
+        const object = Object.fromEntries(objectData)
 
+        const fetchData = await fetch(`http://localhost:8000/api/v1/products/${id}`, {
+            method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(object)
+        })
+        const data = await fetchData.json();
+        alert(data.status)
+    }
+
+    if(formEl){
+        formEl.addEventListener('submit', (event)=>{
+            event.preventDefault();
+            patchItem()
+        })
+    }
 
 }
 export default alterItem
