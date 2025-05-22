3dd321a2b80131071d0d05ade8b5b63b786afbf1
julio2116 <juliobatista2116@gmail.com>
2025-05-22 12:17:05 -0300

    Subindo bloqueio de consumo das rotas para outros sites

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const corsOptions = {
+const corsOptions = {
   origin: process.env.CORS_ORIGIN || 'http://127.0.0.1:5500',
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
+
+export default corsOptions
--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const options = require('../config/corsConfig.js')
+const { corsOptions } = require('../config/corsConfig.js')
 
 const app = express();
 app.use(express.json());
-app.use(cors(options));
+app.use(cors(corsOptions));
 
 const router = require('../routes/routes')
 
